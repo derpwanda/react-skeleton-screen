0c38cd952e206c1f72c4c04687d60568a1224616
Wanda Evans <39149409+derpwanda@users.noreply.github.com>
2020-11-18 13:23:02 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Hello CodeSandbox</h1>
+      </header>
+      <h2>Start editing to see some magic happen!</h2>
+
+      <div className="content"></div>
+    </div>
+  );
+}
