3b37c3e85470370006b3affb920239262283c2d6
Wanda Evans <39149409+derpwanda@users.noreply.github.com>
2020-11-19 17:01:02 -0600

    initial commit

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const User = () => {
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    setTimeout(async () => {
+      const result = await fetch(
+        "https://jsonplaceholder.typicode.com/users/1"
+      );
+      const data = await result.json();
+      setProfile(data);
+    }, 5000);
+  });
+
   return (
     <div className="User">
       <h2>User Profiles</h2>
+      {profile && (
+        <div className="profile">
+          <h3>{profile.username}</h3>
+          <p>{profile.email}</p>
+          <a href={profile.website}>{profile.website}</a>
+        </div>
+      )}
+
+      {!profile && <div>loading...</div>}
     </div>
   );
 };
